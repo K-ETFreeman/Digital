8175e91cd07774467860c16fd9073862ab843df6
ETFreeman <kfreemaneternal@gmail.com>
2020-09-16 13:04:48 +0300

    someBody once toldme

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 //IE HTMLcollection foreach polyfill
@@ -10661,18 +10649,17 @@ var casesSwiper = new Swiper('.cases__swiper', {
   }
 });
 ;
-var mid = document.querySelector('.parallax-container');
-
-var parlx = _toConsumableArray(document.querySelectorAll('.parallax')).map(function (item) {
-  return [item, parseFloat(item.getAttribute('data-depth'))];
+var deflt = document.getElementById('default');
+document.querySelectorAll('.hoverjs').forEach(function (item) {
+  return item.addEventListener('mouseenter', function () {
+    deflt.classList.add('hidden');
+    document.getElementById(item.getAttribute('data-hoverid')).classList.add('hover');
+  });
 });
-
-parlx.forEach(function (item) {
-  return item[0].style = "transform: translateY(".concat(item[1] * (window.innerHeight / 2 - (mid.getBoundingClientRect().top + mid.getBoundingClientRect().bottom) / 2), "px)");
-});
-document.addEventListener('scroll', function () {
-  parlx.forEach(function (item) {
-    return item[0].style = "transform: translateY(".concat(item[1] * (window.innerHeight / 2 - (mid.getBoundingClientRect().top + mid.getBoundingClientRect().bottom) / 2), "px)");
+document.querySelectorAll('.hoverjs').forEach(function (item) {
+  return item.addEventListener('mouseleave', function () {
+    deflt.classList.remove('hidden');
+    document.getElementById(item.getAttribute('data-hoverid')).classList.remove('hover');
   });
 });
 objectFitImages();
